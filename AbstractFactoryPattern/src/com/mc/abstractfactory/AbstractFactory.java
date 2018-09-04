e6c85725e336608d42030602eee1d6b693c45d70
minionscoding <326746295@qq.com>
2018-09-04 15:09:26 +0800

    添加抽象工厂模式

---
+++ AbstractFactoryPattern/src/com/mc/abstractfactory/AbstractFactory.java
@@ -0,0 +1,11 @@
+package com.mc.abstractfactory;
+
+/**
+ * Created by huangying on 2018/9/4.
+ * 为 Color 和 Shape 对象创建抽象类来获取工厂
+ */
+public abstract class AbstractFactory {
+    public abstract Color getColor(String color);
+
+    public abstract Shape getShape(String shape);
+}
