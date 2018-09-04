1ef3189890ae37d413d66108c3e921112eea6ae7
minionscoding <326746295@qq.com>
2018-09-04 17:29:03 +0800

    单例模式的6中写法

---
+++ SingletonPattern/src/com/mc/singleton/SingletonDcl.java
@@ -0,0 +1,32 @@
+package com.mc.singleton;
+
+/**
+ * Created by huangying on 2018/9/4.
+ *
+ * 4、双检锁/双重校验锁（DCL，即 double-checked locking）
+ *
+ * JDK 版本：JDK1.5 起
+ *
+ * 是否 Lazy 初始化：是
+ *
+ * 是否多线程安全：是
+ *
+ * 实现难度：较复杂
+ *
+ * 描述：这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
+ * getInstance() 的性能对应用程序很关键。
+ */
+public class SingletonDcl {
+    private volatile static SingletonDcl singleton;
+    private SingletonDcl (){}
+    public static SingletonDcl getSingleton() {
+        if (singleton == null) {
+            synchronized (SingletonDcl.class) {
+                if (singleton == null) {
+                    singleton = new SingletonDcl();
+                }
+            }
+        }
+        return singleton;
+    }
+}
